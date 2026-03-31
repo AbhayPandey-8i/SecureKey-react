99b3d0fd7dc4621d5c4199ee176948932e4c6746
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-31 14:18:51 +0530

    Bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <div className='bg-slate-800 text-white flex flex-col justify-center items-center bottom-0 w-full'>
             <div className="logo font-bold text-2xl">
-                <span className='text-green-500'> &lt;</span><span>Pass</span>
-                <span className='text-green-500'>OP/&gt;</span>
+                <span className='text-green-500'> &lt;</span><span>Secure</span>
+                <span className='text-green-500'>Key/&gt;</span>
             </div>
             <div className='flex justify-center items-center'>
                 Created with <img className='w-7 mx-2 ' src="icons/heart.png" alt="" /> by Abhay
